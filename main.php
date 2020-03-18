a6f5cab400512af87c8a877b0a9a712307d9449c
ilgiz <62037077+RIM666@users.noreply.github.com>
2020-03-18 21:53:19 +0300

    Update main.php

--- main.php
+++ main.php
@@ -7,9 +7,10 @@ require_once 'config.php';
 
 try {
   echo config('site_name'), PHP_EOL;
-  echo config('db.user', "localhost"), PHP_EOL;
+  echo config('db.user'), PHP_EOL;
   echo config('db.host', 'localhost'), PHP_EOL;
   echo config('db.entrances.april.none'), PHP_EOL;
+  echo config('db.entrances.june'), PHP_EOL;
 } catch (Exception $e) {
   echo $e;
 }
